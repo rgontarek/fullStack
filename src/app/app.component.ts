a37c04a66683406431c52ab0b44ef3e267ad09df
Ryan Gontarek <rgontarek@wisc.edu>
2019-03-18 19:21:56 -0500

    full stack implementation completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public http: HttpClient) {
+
+    // this.http.get('https://jpqkmhpn4k.execute-api.us-east-2.amazonaws.com/production/users/1').subscribe((data) => {
+    //   console.log(data);
+    // });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
-}
 
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title:any = "";
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+
+    this.http.get('https://jpqkmhpn4k.execute-api.us-east-2.amazonaws.com/production/users/3').subscribe((data) => {
+      // console.log(data);
+      this.title = data;
+    });
 
   }
 
